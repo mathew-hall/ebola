66d659ca576833e23c934d2dca1f363dcd4aacf8
Mathew Hall <mathew.james.hall@gmail.com>
2014-10-24 16:13:26 +0100

    Comment existing process and change so all is set to all countries

--- analyses/shinyCountryTimeseries/server.R
+++ analyses/shinyCountryTimeseries/server.R
@@ -16,37 +16,36 @@ url <- "https://raw.githubusercontent.com/cmrivers/ebola/master/country_timeseri
 data <- getURL(url, ssl.verifypeer = FALSE)
 df <- read.csv(textConnection(data))
 
+#Drop the Date col
 df1_noDate <- df[, !names(df) %in% c("Date")]
-day <- c(0:df1_noDate[1, 1])
+#Build a series from 0...latest day in data set
+day <- c(0:max(df1_noDate$Day))
+#We'll add updates on each day we have data for each country here
 df3_merge <- data.frame(day)
 
-theme_set(theme_minimal())
-#Calculate death rates:
-for(i in 2:ncol(df1_noDate)){
-  df_temp <- df1_noDate[, c(1, i)]
+#For each country:
+for(country in 2:ncol(df1_noDate)){
+  df_temp <- df1_noDate[, c(1, country)] #Day,(Cases|Deaths)_Country
+  #Data set is snapshots at day of reporting, with NAs representing "no change"/"no new data"
+  #so ignore those with NAs.
   df_temp <- na.omit(df_temp)
-  last <- nrow(df_temp)
-  last
-  df_temp[last,1]
-  day.offset <- df_temp[last,1]
-  df_temp$day.adj <- df_temp$Day - day.offset
-  df_temp
+
+  #Rescale all series so day 0 == first reported case/death
+  df_temp$day.adj <- df_temp$Day - min(df_temp$Day)
+
   df3_merge <- merge(x = df3_merge, y = df_temp[, names(df_temp) != "Day"],
                      by.x = "day", by.y = "day.adj", all.x = TRUE)
 }
 
 
-
 row.names(df3_merge) <- df3_merge$day
 df3_merge <- df3_merge[, names(df3_merge) != "day"]
+#df3 is day, country, country, country, country, ...
 
-#df4 <- df3_merge %>%
-#    as.matrix() %>%
-#        t() %>%
-#            as.data.frame()
 
 df4 <- as.data.frame(t(as.matrix(df3_merge)))
 
+#split into country, first get names:
 vars <- colsplit(row.names(df4), "_", c("type", "place"))
 df4 <- cbind(vars, df4)
 row.names(df4) <- NULL
@@ -56,7 +55,7 @@ names(df5_melt) <- c("type", "place", "day", "count")
 df5_melt$type[df5_melt$type == "Case"] <- "Cases"
 
 
-all <- c("Guinea", "Liberia", "SierraLeone", "Nigeria", "Senegal")
+all <- unique(df5_melt$place)
 c_colors <- brewer.pal(length(all), 'Set1')
 names(c_colors) <- all
 
