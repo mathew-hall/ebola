db8c7749e28ab864dc68424d08a4e27ad9252a75
Mathew Hall <mathew.james.hall@gmail.com>
2014-10-24 15:10:34 +0100

    Switch to ggvis

--- analyses/shinyCountryTimeseries/ui.R
+++ analyses/shinyCountryTimeseries/ui.R
@@ -18,14 +18,15 @@ shinyUI(fluidPage(
                                             "Senegal" = "Senegal",
                                             "All" = "All"),
                                         selected = "All"),
-                     checkboxInput("log", "Plot y-axis on log scale")
+                     checkboxInput("log", "Plot y-axis on log scale"),
+                     uiOutput("plot_controls")
                      ),
 
         mainPanel(p("This graphs the cases and deaths of each country and normalizes the onset dates all to '0' so countries can be compared"),
                   "Data was all taken from Caitlin River's 'ebola' repository",
                   a('here', href = 'https://github.com/cmrivers/ebola'),
 
-                  plotOutput("plot")
+                  ggvisOutput("plot")
                   )
         )
 ))
