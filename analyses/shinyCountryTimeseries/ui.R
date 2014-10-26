65e230e0b9d50fae6477edfbfa641257235ae2ea
Mathew Hall <mathew.james.hall+gh@gmail.com>
2014-10-26 20:12:57 +0000

    Fix link to chendaniely's original repo in UI

--- analyses/shinyCountryTimeseries/ui.R
+++ analyses/shinyCountryTimeseries/ui.R
@@ -21,7 +21,7 @@ shinyUI(fluidPage(
         mainPanel(p("These graphs show incidence and death rates of Ebola in countries where infections have been reported."),
                   p("Data was all taken from Caitlin River's",
                   a('"ebola" repository', href = 'https://github.com/cmrivers/ebola'),
-                  "and this interface is forked from ", a("Daniel Chen's","https://github.com/chendaniely/ebola"),"app."),
+                  "and this interface is forked from ", a("Daniel Chen's",href="https://github.com/chendaniely/ebola"),"app."),
 
                   ggvisOutput("plot_cases"),
                   ggvisOutput("plot_deaths")
